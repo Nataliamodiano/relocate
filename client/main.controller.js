056358b8f69e0c0f1f34d63265ca44dba63d38ef
natalia <nataliamodiano@gmail.com>
2016-02-01 14:09:42 -0800

    Got map to work, and display indeed and yelp markers, #3

--- client/main.controller.js
+++ client/main.controller.js
@@ -6,7 +6,7 @@ angular.module('relocate')
         .then(function(response) {
           vm.businesses = response.data.businesses;
           console.log(vm.businesses);
-
+          // get walk score for yelp location
           for (i = 0; i < vm.businesses.length; i++){
             lat = vm.businesses[i].location.coordinate.latitude;
             long = vm.businesses[i].location.coordinate.longitude;
@@ -14,25 +14,17 @@ angular.module('relocate')
               .then(function(response) {
               vm.scoreData = response.data;
               console.log(vm.scoreData);
-            });
+            }); 
           }
-
+          // map
           NgMap.getMap().then(function(map) {
-            console.log(map.getCenter());
-            console.log('markers', map.markers);
-            console.log('shapes', map.shapes);
-          });
+              vm.map = map;
+            });
       });
       indeedService.jobs($scope.keyword, $scope.location)
         .then(function(response) {
           vm.results = response.data.results;
           console.log(vm.results);  
-        
-          NgMap.getMap().then(function(map) {
-            console.log(map.getCenter());
-            console.log('markers', map.markers);
-            console.log('shapes', map.shapes);
-          });
         });
       }
   });
--- server/public/all.js
+++ server/public/all.js
@@ -1,5 +1,4 @@
 angular.module('relocate', ['ngRoute', 'ngMap']);
-// angular.module('myApp', ['ngMap']);
 angular.module('relocate')
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
@@ -33,7 +32,7 @@ angular.module('relocate')
         .then(function(response) {
           vm.businesses = response.data.businesses;
           console.log(vm.businesses);
-
+          // get walk score for yelp location
           for (i = 0; i < vm.businesses.length; i++){
             lat = vm.businesses[i].location.coordinate.latitude;
             long = vm.businesses[i].location.coordinate.longitude;
@@ -41,89 +40,20 @@ angular.module('relocate')
               .then(function(response) {
               vm.scoreData = response.data;
               console.log(vm.scoreData);
-            });
+            }); 
           }
-
+          // map
           NgMap.getMap().then(function(map) {
-            console.log(map.getCenter());
-            console.log('markers', map.markers);
-            console.log('shapes', map.shapes);
-          });
+              vm.map = map;
+            });
       });
       indeedService.jobs($scope.keyword, $scope.location)
         .then(function(response) {
           vm.results = response.data.results;
           console.log(vm.results);  
-        
-          NgMap.getMap().then(function(map) {
-            console.log(map.getCenter());
-            console.log('markers', map.markers);
-            console.log('shapes', map.shapes);
-          });
         });
       }
   });
-
-  // .controller('mapController', function($http, NgMap) {
-  //     var vm = this;
-  //     vm.positions = [
-  //       [54.779951, 9.334164], [47.209613, 15.991539],
-  //       [51.975343, 7.596731], [51.97539, 7.596962], 
-  //       [47.414847, 8.23485], [47.658028, 9.159596],
-  //       [47.525927, 7.68761], [50.85558, 9.704403],
-  //       [54.320664, 10.285977], [49.214374, 6.97506],
-  //       [52.975556, 7.596811], [52.975556, 7.596811],
-  //       [52.975556, 7.596811], [52.975556, 7.596811], 
-  //       [52.975556, 7.596811], [52.975556, 7.596811],
-  //       [52.975556, 7.596811], [52.975556, 7.596811],
-  //       [52.975556, 7.596811], [52.975556, 7.596811]];
-        
-  //       vm.dynMarkers = []
-  //       NgMap.getMap().then(function(map) {
-  //         var bounds = new google.maps.LatLngBounds();
-  //         for (var k in map.customMarkers) {
-  //           var cm = map.customMarkers[k];
-  //           vm.dynMarkers.push(cm);
-  //           bounds.extend(cm.getPosition());
-  //         };
-          
-  //         vm.markerClusterer = new MarkerClusterer(map, vm.dynMarkers, {});
-  //         map.setCenter(bounds.getCenter());
-  //         map.fitBounds(bounds);  
-  //      });
-  //   });
-
-
-// var app = angular.module('myApp', ['ngMap']);
-
-// app.controller('mapController', function($http, $interval, NgMap) {
-//   var vm = this;
-//   vm.positions = [
-//     [54.779951, 9.334164], [47.209613, 15.991539],
-//     [51.975343, 7.596731], [51.97539, 7.596962], 
-//     [47.414847, 8.23485], [47.658028, 9.159596],
-//     [47.525927, 7.68761], [50.85558, 9.704403],
-//     [54.320664, 10.285977], [49.214374, 6.97506],
-//     [52.975556, 7.596811], [52.975556, 7.596811],
-//     [52.975556, 7.596811], [52.975556, 7.596811], 
-//     [52.975556, 7.596811], [52.975556, 7.596811],
-//     [52.975556, 7.596811], [52.975556, 7.596811],
-//     [52.975556, 7.596811], [52.975556, 7.596811]];
-    
-//     vm.dynMarkers = []
-//     NgMap.getMap().then(function(map) {
-//       var bounds = new google.maps.LatLngBounds();
-//       for (var k in map.customMarkers) {
-//         var cm = map.customMarkers[k];
-//         vm.dynMarkers.push(cm);
-//         bounds.extend(cm.getPosition());
-//       };
-      
-//       vm.markerClusterer = new MarkerClusterer(map, vm.dynMarkers, {});
-//       map.setCenter(bounds.getCenter());
-//       map.fitBounds(bounds);  
-//    });
-// });
 angular.module('relocate')
   .factory('walkScoreService', function($http) {
     var getWalkScore = function(lat, long) {
